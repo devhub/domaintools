daf2736a27461f5e1ac950d48a0949816593efcc
Daniel Rust <dlrust@gmail.com>
2015-01-04 12:41:30 -0800

    Bumped version to 0.3.7

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='domaintools',
-    version='0.3.6',
+    version='0.3.7',
     description='Domain parsing with python',
     author='DevHub',
     url='http://github.com/devhub/domaintools',
